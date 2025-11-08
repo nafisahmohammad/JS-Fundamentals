613eda8d9ccc59da2b0219cbd066de1618bd12d6
nafisahmohammad <nafisahmohammad1616@gmail.com>
2025-11-08 22:56:46 +0100

    update

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
-const args = ProcessingInstruction.argv.length -2;
+const args = Process.argv.length -2;
 if (args === 0) {
     console.log ('No argument');
 } else if (args === 1) {
     console.log ('Argument found');
 } else {
-    console.log ('Argument found');
+    console.log ('Arguments found');
 }
