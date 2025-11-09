e17fbbf780c1e0f95400bb0107f9ebe1dc5121c4
nafisahmohammad <nafisahmohammad1616@gmail.com>
2025-11-09 08:46:53 +0100

    JS

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/mode
 
-const languages = ['c is fun', 'python is cool', 'JavaScript is amazing'];
+const languages = ['c is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < languages.length; i++ ) {
     console.log(languages[i]);
--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/mode
-const x = parseInt(process.argv[2]);
+const x = parseInt(process.argv [2]);
 
 if (isNaN(x)) {
     console.log ('missing number of occurrences')
 
 } else {
   for ( let i =0; i < x; i++) {
-    console.log ('c is fun');
+    console.log ('C is fun');
   }
 }
